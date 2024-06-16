343d42228ef77821957d6ccf9a507add0874cf65
Sebastian Garcia Muñoz <sebas@AirdeSebastian.home>
2024-06-17 00:14:54 +0200

    Restrict mode off

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,7 @@ import { UserProvider } from './context/userContext'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <UserProvider>
     <RouterProvider router={router} />
     </UserProvider>
-  </React.StrictMode>,
 )
